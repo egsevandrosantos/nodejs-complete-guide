39cffcd2c8e8081ed55e1bae027dd2b1096995e5
Evandro Santos <egs.evandro.santos@gmail.com>
2019-10-13 17:12:39 -0300

    Blocking and Non-Blocking Code

--- app.js
+++ app.js
@@ -30,10 +30,11 @@ const server = http.createServer((req, res) => {
         return req.on("end", () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fileSystem.writeFileSync('message.txt', message);
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
+            fileSystem.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         });
     }
     res.write('<html>');
