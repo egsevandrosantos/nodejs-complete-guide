2b1dd0170ca404c41fcba4103e17f567e00878a9
Evandro Santos <egs.evandro.santos@gmail.com>
2019-10-13 12:04:39 -0300

    Sending Responses

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head>');
+    res.write('<title>My first page</title>');
+    res.write('</head>');
+    res.write('<body>');
+    res.write('<h1>Hello World! My NodeJS Server!!!</h1>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
