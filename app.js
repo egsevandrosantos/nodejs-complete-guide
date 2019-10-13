57a5b7e90abb43d4de7b7d50c9a4b4a8b7b371b0
Evandro Santos <egs.evandro.santos@gmail.com>
2019-10-13 12:15:43 -0300

    Routing Requests

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
     // process.exit();
+    const url = req.url;
     res.setHeader('Content-Type', 'text/html');
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head>');
+        res.write('<title>Enter Message</title>');
+        res.write('</head>');
+        res.write('<body>');
+        res.write('<form action="/message" method="POST">');
+        res.write('<label for="message">Write your message</label>');
+        res.write('<input id="message" name="message" type="text" />');
+        res.write('<button type="submit">Send</button>');
+        res.write('</form>');
+        res.write('</body>');
+        res.write('</html>');       
+        return res.end();   
+    }
     res.write('<html>');
     res.write('<head>');
     res.write('<title>My first page</title>');
