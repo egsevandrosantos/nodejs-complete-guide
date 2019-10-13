b0948f5c3167cdef57751f725ff0f4be6965f54f
Evandro Santos <egs.evandro.santos@gmail.com>
2019-10-13 11:56:51 -0300

    Understanding Requests

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(res);
+    console.log(req.url, req.method, req.headers);
     // process.exit();
 });
 
